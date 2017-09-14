de13e01859333b22b314191ee52724f677fe1e29
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-09-14 14:51:14 -0400

    yes

--- Duel/src/duel/CharacterB.java
+++ Duel/src/duel/CharacterB.java
@@ -1,5 +1,13 @@
 package duel;
 
-public class CharacterB {
-
+public class CharacterB implements Dueler{
+	
+	public CharacterB() {
+		public void taunt() {
+			System.out.println("You will lose");
+		}
+		public String getName(){
+			System.out.println("CharacterA");
+		}
+	}
 }
