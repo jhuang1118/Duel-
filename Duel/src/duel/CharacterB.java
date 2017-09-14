e6ef80c1655bd8f886893c8d0571c16d1f62c3df
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-09-14 15:35:22 -0400

    yeah

--- Duel/src/duel/CharacterB.java
+++ Duel/src/duel/CharacterB.java
@@ -1,13 +1,19 @@
 package duel;
 
-public class CharacterB implements Dueler{
+public  class CharacterB implements Dueler{
 	
 	public CharacterB() {
-		public void taunt() {
-			System.out.println("You will lose");
-		}
-		public String getName(){
-			System.out.println("CharacterA");
-		}
+		
+	}
+	public void taunt() {
+		String taunting = "You will lose";
+		System.out.println(taunting);
+	}
+	public String getName(){
+		String name = "Johnson";
+		System.out.println("name");
+	}
+	public boolean determineIfOpponentIsFair(Dueler d, int target) {
+		return true;
 	}
 }
