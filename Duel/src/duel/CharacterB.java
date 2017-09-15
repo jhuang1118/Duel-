a3c4c5a1f79e16fc149b23ec0f3bf777cde127d5
Jas <Jas@jasmits-mbp.home>
2017-09-14 23:20:08 -0400

    k

--- Duel/src/duel/CharacterB.java
+++ Duel/src/duel/CharacterB.java
@@ -2,12 +2,25 @@ package duel;
 
 public class CharacterB implements Dueler{
 	
-	public CharacterB() {
+	private String name;
+	public CharacterB() {}
 		public void taunt() {
 			System.out.println("You will lose");
 		}
 		public String getName(){
-			System.out.println("CharacterA");
+			name = "Jas";
+			return name;		}
+		public void setStartingHP(int hp) {
+			System.out.println(hp);
 		}
-	}
+		public int getHP() {
+			return hp;
+		}
+		public boolean determineIfOpponentIsFair(Dueler d, int target) {
+			return true;
+		}
 }
+
+
+
+
