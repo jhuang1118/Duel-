0862060474eba523ca14828bd2b79bad9f4b7dfe
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2017-09-27 15:08:58 -0400

    duel charA

--- Duel/src/duel/CharacterA.java
+++ Duel/src/duel/CharacterA.java
@@ -2,24 +2,84 @@ package duel;
 
 public class CharacterA implements Dueler {
 	private String name;
-	private int hp;
-	public CharacterA() {}
+	private int currentHP;
+	private boolean isLoaded;
+	
+	String[] phrases = {"You will lose!","Die!","Experience tranquility","Yoooooooooo!"};
+	public CharacterA() {
+		this.name = "Ryu";
+	}
 		public void taunt() {
-			String[]phrases = {"You will lose!","Die!","Experience tranquility","Yoooooooooo!"};
-			int pickRandom = (int) ((Math.random() * 4 ) + 0);
-			System.out.println(phrases[pickRandom]);
+			int rng = (int)(Math.random() * phrases.length);
+			if(rng != 1) {
+				System.out.println("Ryu says, '" + phrases[rng] + "'");
+			}
+			else {
+				System.out.println(phrases[rng]);
+			}
 		}
 		public String getName() {
-			name = "Johnson";
 			return name;
 		}
 		public void setStartingHP(int hp) {
-			System.out.println(hp);
+			currentHP = hp;
+			isLoaded = false;
 		}
 		public int getHP() {
-			return hp;
+			return currentHP;
 		}
 		public boolean determineIfOpponentIsFair(Dueler d, int target) {
+			if(d.getHP() != currentHP) {
+				return false;
+			}
 			return true;
 		}
+		public int getAction(Object caller) {
+			int rngAction = (int)(Math.random()*10);
+			
+			if(caller instanceof Duel) {
+				if(this.currentHP >= 20) {
+					if(rngAction < 8) {
+						return 2;
+					}
+					else if (rngAction < 2) {
+						this.isLoaded = true;
+						return 0;
+					}
+					else {
+						return shoot();
+					}
+				}
+				else {
+					if(rngAction <= 5) {
+						return 0;
+					}
+					else {
+						return shoot();
+					}
+				}
+				
+			}
+			return 3;
+				
+		}
+		public void hit(Object caller) {
+			if(caller instanceof Duel) {
+				this.currentHP -= 10;
+			}
+				
+		}
+		public int shoot() {
+			if(isLoaded == true) {
+				isLoaded = false;
+				return 1;
+			}
+			else {
+				int action = (int)(Math.random() * 2);
+				if(action == 0) {
+					return action;
+				}
+				return 2;
+			}
 	}
+}
